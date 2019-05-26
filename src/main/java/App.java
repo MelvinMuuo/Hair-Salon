a46e021b964e09576535e12d6b07e44c0f0c02aa
MelvinMuuo <muuomelvin73@gmail.com>
2019-05-26 17:36:28 +0300

    add stylist.vtl for displaying list of stylists

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,8 @@
 import java.util.HashMap;
 import java.util.Map;
 import spark.ModelAndView;
-import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+import spark.template.velocity.VelocityTemplateEngine;
 
 public class App {
   public static void main(String[] args) {
@@ -13,5 +13,38 @@ public class App {
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/stylists/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/stylist-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/stylists", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      int age = Integer.parseInt(request.queryParams("age"));
+      int year = Integer.parseInt(request.queryParams("year"));
+      int rate = Integer.parseInt(request.queryParams("rate"));
+      Stylist newStylist = new Stylist(name, age, year, rate);
+      newStylist.save();
+      model.put("template", "templates/stylist-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/stylists", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("stylists", Stylist.all());
+      model.put("template", "templates/stylists.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/stylists", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("stylists", Stylist.all());
+      model.put("template", "templates/stylists.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 }
