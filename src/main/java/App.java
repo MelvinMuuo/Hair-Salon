4771aa3268673f7efd28c6cbae9410bb033ef550
MelvinMuuo <muuomelvin73@gmail.com>
2019-05-27 14:43:27 +0300

    add black header and image of logo

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,7 @@ import spark.template.velocity.VelocityTemplateEngine;
 
 public class App {
   public static void main(String[] args) {
+    staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {
